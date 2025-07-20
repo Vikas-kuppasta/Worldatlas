0e3996d3173f4df51f06be5fb9a065333b0a2e98
Vikas-kuppasta <your-vikaskuppasta@gmail.com>
2025-07-20 23:51:46 +0530

    added one more time

--- src/Pages/herosection.jsx
+++ src/Pages/herosection.jsx
@@ -4,7 +4,7 @@ import banner from './banner.jpg'
 
 export default function Herosection() {
     return (
-                <main className=' flex pl-20 pr-20 pt-5 pb-5 mt-5 items-center justify-between'>
+                <main className=' flex  pl-20 pr-20 pt-5 pb-5 mt-15 items-center justify-between'>
                     <div className='w-1/2'>
                         <h1 className='text-white font-mony font-bold text-5xl leading-13 '>Explore the World, One country at a Time. </h1>
                         <p className='text-white text-left font-mony mt-6 mb-9 '>"Explore the world and discover breathtaking landscapes, diverse cultures, and unforgettable experiences that open your mind, spark curiosity, and enrich your soul with endless wonder."
